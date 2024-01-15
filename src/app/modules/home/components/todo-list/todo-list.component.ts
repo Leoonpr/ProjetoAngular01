a22ae9bba758dd2b1bd4519f51217a8b100f4178
leonardo <rochaleo879@gmail.com>
2024-01-14 22:38:23 -0300

    feat: cria a função de deletar todas as tasks

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.component.scss'
+  styleUrl: './todo-list.component.scss',
 })
 export class TodoListComponent {
+  public taskList: Array<TaskList> = [
+    { task: 'First', checked: true },
+    { task: 'Second', checked: false },
+  ];
 
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAll() {
+    this.taskList = [];
+  }
 }
